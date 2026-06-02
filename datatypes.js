8c38db2491bb5292eba49b84423bf5e157da66f3
Bilal <bbaig0037@gmail.com>
2026-06-02 16:17:21 +0500

    datatypes practice

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+//number>  2 ......
+//string> "Hello"
+//float> 3.14
+//boolean> true or false
+//null> represents the intentional absence of any object value
+//undefined> represents a variable that has been declared but has not been assigned a value
+//symbol> a unique value that can be used as an identifier for object properties
+let name="Bilal";
+let state; //undefined
+let temp=null; //null
+console.log(typeof name);
