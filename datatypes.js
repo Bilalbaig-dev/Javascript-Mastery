19c65a754f0b35afa9a4ce921a7193a3c1d8ae2c
Bilal <bbaig0037@gmail.com>
2026-06-02 16:44:29 +0500

    conversion

--- datatypes.js
+++ datatypes.js
@@ -9,3 +9,27 @@ let name="Bilal";
 let state; //undefined
 let temp=null; //null
 console.log(typeof name);
+
+
+/*Conversion in data types */
+let greet="10";
+console.log(typeof greet); //string
+let numberConversion=Number(greet);
+console.log(typeof numberConversion); //number
+
+// "10" > number
+// "10abc" > NaN (Not a Number)
+// "abc" > NaN
+// null > 0
+// undefined > NaN
+
+let num;
+let stringConversion=String(num);
+console.log(typeof stringConversion); //string
+console.log(stringConversion); //undefined
+
+// 10 > "10"
+// 3.14 > "3.14"
+// true > "true"
+// null > "null"
+// undefined > "undefined"
